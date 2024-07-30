1d910374b41aca5aa1070d75988623f87005a3a4
stormyhs <stormy.hs@protonmail.com>
2024-07-30 21:35:38 +0200

    refactor: slightly more informative variable name

--- src/engine.rs
+++ src/engine.rs
@@ -3,26 +3,26 @@ use std::collections::HashMap;
 use crate::tokens::Expr;
 
 pub fn evaluate(expr: Vec<Expr>, state: &HashMap<String, i16>) -> i16 {
-    let mut result = 0;
+    let mut return_value = 0;
     let mut state = state.clone();
 
     for e in expr {
         match e {
             Expr::Number(n) => {
-                result = n;
+                return_value = n;
             },
             Expr::String(_s) => {
-                panic!("#! Unable to evaluate string");
+                return_value = 0;
             },
             Expr::BinOp(op, left, right) => {
                 let left = evaluate(vec![*left], &state);
                 let right = evaluate(vec![*right], &state);
 
                 match op {
-                    '+' => result = left + right,
-                    '-' => result = left - right,
-                    '*' => result = left * right,
-                    '/' => result = left / right,
+                    '+' => return_value = left + right,
+                    '-' => return_value = left - right,
+                    '*' => return_value = left * right,
+                    '/' => return_value = left / right,
                     '=' => {
                         state.insert("x".to_string(), right);
                     }
@@ -32,6 +32,6 @@ pub fn evaluate(expr: Vec<Expr>, state: &HashMap<String, i16>) -> i16 {
         }
     }
 
-    return result;
+    return return_value;
 }
 
